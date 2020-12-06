19c252523930c985cc2c84df935b936481e308ab
Dezmou <incredibledez@gmail.com>
2020-12-06 11:07:13 +0000

    wip

--- main.c
+++ main.c
@@ -9,19 +9,19 @@
 
 // int *image;
 
-void uploadImage(int *data){
+void uploadImage(int *data)
+{
   // image = data;
 }
 
-
-void processImage(int *data, int width, int height)
+void processImage(int *data, int width, int height, int amount)
 {
   for (int y = 0; y < height; y += 1)
   {
     for (int x = 0; x < width; x += 1)
     {
       unsigned char *pix = PIX(x, y);
-      pix[RED] = 200;
+      pix[RED] = (unsigned char *)amount;
       pix[GREEN] = 0;
       pix[BLUE] = 0;
     }
