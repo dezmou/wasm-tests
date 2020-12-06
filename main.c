5ec095c1558c63930f7b7bfe14e7bada96427b78
Dezmou <incredibledez@gmail.com>
2020-12-06 11:11:09 +0000

    wip

--- main.c
+++ main.c
@@ -7,23 +7,19 @@
 
 #define PIX(x, y) ((unsigned char *)&(data[(y * width + x)]))
 
-// int *image;
-
-void uploadImage(int *data)
-{
-  // image = data;
-}
-
 void processImage(int *data, int width, int height, int amount)
 {
   for (int y = 0; y < height; y += 1)
   {
     for (int x = 0; x < width; x += 1)
     {
-      unsigned char *pix = PIX(x, y);
-      pix[RED] = (unsigned char)amount;
-      pix[GREEN] = 0;
-      pix[BLUE] = 0;
+      if (y % amount == 0)
+      {
+        unsigned char *pix = PIX(x, y);
+        pix[RED] = (unsigned char)amount;
+        pix[GREEN] = 0;
+        pix[BLUE] = 0;
+      }
     }
   }
 }
