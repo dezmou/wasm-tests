edae6fc8983c800e90760e70c8e61fb438def4eb
Dezmou <incredibledez@gmail.com>
2020-12-06 11:39:31 +0000

    wip

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 
 #define RED 0
 #define GREEN 1
@@ -7,18 +6,17 @@
 
 #define PIX(x, y) ((unsigned char *)&(data[(y * width + x)]))
 
-void processImage(int *data, int width, int height, int amount)
+void processImage(int *data, int width, int height, unsigned char amount)
 {
   for (int y = 0; y < height; y += 1)
   {
     for (int x = 0; x < width; x += 1)
     {
-      if (y % amount == 0)
       {
         unsigned char *pix = PIX(x, y);
-        pix[RED] = (unsigned char)amount;
-        pix[GREEN] = 0;
-        pix[BLUE] = 0;
+        pix[RED] = amount;
+        pix[GREEN] = 250 - amount;
+        pix[BLUE] = 250 - amount;
       }
     }
   }
